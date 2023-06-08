0e5a1001bd342021d449ac3b27d958f7a00754d2
kyeon06 <jkyeon06@gmail.com>
2023-06-08 17:49:55 +0900

    :sparkles: create app endpoints and serializer

---
+++ backend/server/apps/endpoints/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# endpoint information
+class Endpoint(models.Model):
+
+    name = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+# 서비스에 사용되는 알고리즘에 대한 정보
+class MLAlgorithm(models.Model):
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    owner = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+
+# 알고리즘 상태에 대한 정보
+class MLAlgorithmStatus(models.Model):
+
+    status = models.CharField(max_length=128)
+    active = models.BooleanField()
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name = "status")
+
+# 모든 알고리즘 요청에 대한 정보(모니터, A/B 테스트)
+class MLRequest(models.Model):
+
+    input_data = models.CharField(max_length=10000)
+    full_response = models.CharField(max_length=10000)
+    response = models.CharField(max_length=10000)
+    feedback = models.CharField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
